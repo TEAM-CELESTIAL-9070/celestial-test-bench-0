ec6be2cdc1e3c3f6d65eabc8245898f6e4c4a2d3
kaan8562 <north45.my@gmail.com>
2024-12-09 23:46:20 +0300

    [HW3] Add member: {KaanKanyılmaz}

---
+++ src/main/java/co/xreos/celestialtestbench0/member/registry/KaanKanyilmaz.java
@@ -0,0 +1,18 @@
+package co.xreos.celestialtestbench0.member.registry;
+
+import co.xreos.celestialtestbench0.member.entities.IMember;
+
+public class KaanKanyilmaz implements IMember {
+    public String getName(){
+        return "KaanKanyılmaz";
+    }
+    public String getId(){
+        return "202";
+    }
+    public String getSubTeam(){
+        return "Celestial";
+    }
+    public long getNetworkId(){
+        return 0;
+    }
+}
