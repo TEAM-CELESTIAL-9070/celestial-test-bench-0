042028384e1a02b612efd31bc8d8421e8da01501
Batuhan Cengiz <batuhancengiz2009@gmail.com>
2024-12-10 23:27:35 +0300

    add Batuhan

---
+++ src/main/java/co/xreos/celestialtestbench0/member/entities/BatuhanCengiz.java
@@ -0,0 +1,25 @@
+package co.xreos.celestialtestbench0.member.entities;
+
+
+public class BatuhanCengiz implements IMember {
+
+        @Override
+        public String getName() {
+            return "Batuhan Cengiz";
+        }
+
+        @Override
+        public String getSubTeam() {
+            return "CELESTIAL";
+        }
+
+        @Override
+        public String getId() {
+            return "402";
+        }
+
+        @Override
+        public long getNetworkId() {
+            return 0;
+        }
+    }
