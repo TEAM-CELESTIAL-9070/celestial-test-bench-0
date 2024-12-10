bfc7185044d2415dd1e56b49957f7531835866bd
cınar <Cinarbuzkurt9@gmail.com>
2024-12-10 18:33:08 +0300

    [HW3] Add member: {Çınar Buzkurt}

---
+++ src/main/java/co/xreos/celestialtestbench0/member/entities/Cinarbuzkurt.java
@@ -0,0 +1,24 @@
+package co.xreos.celestialtestbench0.member.entities;
+
+public class Cinarbuzkurt implements IMember{
+    @Override
+    public String getName() {
+        return "CinarBuzkurt";
+    }
+
+    @Override
+    public String getSubTeam() {
+        return "Yazilim";
+    }
+
+    @Override
+    public String getId() {
+        return "786";
+    }
+
+    @Override
+    public long getNetworkId() {
+        return 0;
+    }
+}
+
